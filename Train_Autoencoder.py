ee6efbba3cc7312ae61021cea60773688c8d97ea
Jingwei <jingwei4@andrew.cmu.edu>
2019-04-16 20:11:51 -0400

    LeNet and PCANet

---
+++ Train_Autoencoder.py
@@ -0,0 +1,68 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+
+
+data = np.load('res.npy')
+use_cuda = torch.cuda.is_available()
+num_epochs = 500
+batch_size = 128
+learning_rate = 1e-3
+
+dataset = data, data
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(500, 128),
+            nn.Tanh(),
+            nn.Linear(128, 64))
+        self.decoder = nn.Sequential(
+            nn.Linear(64, 128),
+            nn.Tanh(),
+            nn.Linear(128, 500),
+            nn.ReLU())
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+model = autoencoder()
+if use_cuda:
+    model = model.cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(
+    model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for data in dataloader:
+        optimizer.zero_grad()
+        features, _ = data
+        if use_cuda:
+            features = Variable(features).cuda()
+        # ===================forward=====================
+        output = model(features)
+        loss = criterion(output, features)
+        tooutput = output[0].detach().numpy()
+        tofeatures = features[0].detach().numpy()
+        # ===================backward====================
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    print('epoch [{}/{}], loss:{:.4f}'
+          .format(epoch + 1, num_epochs, loss.data.item()))
+    print(tofeatures[:10])
+    print(tooutput[:10])
+    print('distance: ', np.linalg.norm(tofeatures-tooutput))
+    print('---------')
+
+
+torch.save(model, 'AEC.pth')
