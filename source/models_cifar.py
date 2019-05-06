18b4560cb1f95a568429d8457fda88e699187335
Chang Liu <changli4@andrew.cmu.edu>
2019-05-06 15:08:38 -0400

    fix: cifar101

--- source/models_cifar.py
+++ source/models_cifar.py
@@ -6,6 +6,42 @@ from torch.autograd.function import Function
 
 from models_utils import *
 
+class CIFARModel01(nn.Module):
+    def __init__(self):
+        super(CIFARModel01, self).__init__()
+        self.th1 = nn.Threshold(0.5, 0)
+        self.th2 = nn.Threshold(-0.5, 1)
+        self.conv1 = nn.Conv2d(3, 64, 3)
+        self.conv2 = nn.Conv2d(64, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 128, 3)
+
+        self.fc1 = nn.Linear(3200, 256)
+        self.dropout1 = nn.Dropout(0.5, inplace=True)
+        self.fc2 = nn.Linear(256, 256)
+        # self.dropout2 = nn.Dropout(0.5, inplace = True)
+        self.fc3 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = self.th1(x)
+        x = -x
+        x = self.th2(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.max_pool2d(x, 2, 2)
+
+        x = x.view(x.size(0), -1)  # flatten
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = F.relu(self.fc2(x))
+        # x = self.dropout2(x)
+        x = self.fc3(x)
+
+        return x
+
 
 class CIFARModel(nn.Module):
     def __init__(self):
