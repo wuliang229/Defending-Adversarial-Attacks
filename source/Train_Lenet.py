eb048027d45bb67f1e5404fb8deaf8d59ee3d942
Chang Liu <changli4@andrew.cmu.edu>
2019-05-05 23:42:11 -0400

    fix: remove normaliztion

--- source/Train_Lenet.py
+++ source/Train_Lenet.py
@@ -19,9 +19,8 @@ best_loss = 1e4
 root = '../data'
 if not os.path.exists(root):
     os.mkdir(root)
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
-train_set = dset.MNIST(root=root, train=True, transform=trans, download=True)
-test_set = dset.MNIST(root=root, train=False, transform=trans, download=True)
+train_set = dset.MNIST(root=root, train=True, transform=transforms.ToTensor(), download=True)
+test_set = dset.MNIST(root=root, train=False, transform=transforms.ToTensor(), download=True)
 train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
 
--- source/Train_Lenet_ANL.py
+++ source/Train_Lenet_ANL.py
@@ -14,15 +14,14 @@ use_cuda = torch.cuda.is_available()
 # params
 batch_size = 128
 best_loss = 1e4
-noise_factor = 0.02
+noise_factor = 0.05
 
 # dataset
 root = '../data'
 if not os.path.exists(root):
     os.mkdir(root)
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
-train_set = dset.MNIST(root=root, train=True, transform=trans, download=True)
-test_set = dset.MNIST(root=root, train=False, transform=trans, download=True)
+train_set = dset.MNIST(root=root, train=True, transform=transforms.ToTensor(), download=True)
+test_set = dset.MNIST(root=root, train=False, transform=transforms.ToTensor(), download=True)
 train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
 
